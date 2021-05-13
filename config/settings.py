b681d56f5de24e29115c4602e6018701c9f00dad
s0019043 <80893402+s0019043@users.noreply.github.com>
2021-05-13 14:16:34 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-doe%l_+^+jcum$7#_-ev1w8(odbabdg6*@wvro@fx4cq^8#^s0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static_files')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
